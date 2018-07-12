537efcb403b78682f80bbca3ab3c0715ecebd46f
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-07-12 09:17:06 -0400

    Add function to make Jukes-Cantor rate matrix.

--- Main.hs
+++ Main.hs
@@ -3,8 +3,18 @@ import Data.List
 import System.Environment
 import Bio.Fasta
 
+
+import Numeric.LinearAlgebra as LA
+
+jc69_line n i = (replicate i (fromIntegral 1::Double))
+                ++[fromIntegral (-(n-1))::Double]
+                ++(replicate (n-1-i) (fromIntegral 1::Double))
+
+jc69 n = (n><n) (concat [jc69_line n i | i <- [0..n-1]]) :: Matrix R
+
 main = do
   args <- getArgs
   let filename = args!!0
   fastas <- read_fasta filename
   putStr $ concatMap show fastas
+  putStr $ show $ jc69 4
